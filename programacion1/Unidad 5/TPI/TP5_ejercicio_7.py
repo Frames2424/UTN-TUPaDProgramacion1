d6079e9844c2ec146474240c8469dceee5e97e0b
Frames2424 <81182446+Frames2424@users.noreply.github.com>
2026-09-12 16:17:44 -0300

    fastsaveejer7

---
+++ programacion1/Unidad 5/TPI/TP5_ejercicio_7.py
@@ -0,0 +1,12 @@
+''''Crear una matriz (lista anidada) de 7x2 con las temperaturas mínimas y máximas de
+una semana.
+● Calcular el promedio de las mínimas y el de las máximas.
+● Mostrar en qué día se registró la mayor amplitud térmica.'''
+
+#Crear matriz anidad como se hace?
+import random
+temp_random = ran
+for i in range(7):
+    for x in range(2):
+        temperatura_max = []
+        temperatura_min = []
